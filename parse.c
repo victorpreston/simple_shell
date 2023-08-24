2bb28808e1acfd9883bab2f1682f25f72b25ac9b
Victor Preston <prestonvictor25@gmail.com>
2023-08-24 06:40:00 -0700

    feat: implement advanced tokenization and command parsing engine

---
+++ parse.c
@@ -0,0 +1,50 @@
+#include "hsh.h"
+
+/**
+ * parse - parse input
+ * @info: shell information
+ *
+ * Description: This function expands aliases, variables, and word splitting
+ *
+ * Return: the final number of tokens
+ */
+int parse(info_t *info)
+{
+	char **tokens, *tok;
+	size_t n = 0;
+	cmdlist_t *cmd = info->commands = cmd_to_list(info->line);
+
+	while (cmd)
+	{
+		remove_comments(cmd);
+		if (!cmd->tokens)
+		{
+			cmd = cmd->next;
+			del_cmd(&(info->commands), n);
+			continue;
+		}
+		expand_aliases(info->aliases, &(cmd->tokens));
+		if (!cmd->tokens)
+		{
+			cmd = cmd->next;
+			del_cmd(&(info->commands), n);
+			continue;
+		}
+		expand_vars(info, &(cmd->tokens));
+		if (!cmd->tokens)
+		{
+			cmd = cmd->next;
+			del_cmd(&(info->commands), n);
+			continue;
+		}
+		tokens = cmd->tokens;
+		for (tok = *tokens; tok; tok = *(++tokens))
+		{
+			*tokens = dequote(tok);
+			free(tok);
+		}
+		cmd = cmd->next;
+		++n;
+	}
+	return (n);
+}
