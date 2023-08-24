f7c14076dfcfe90e2dcc1e4f179339a0bea420ff
Victor Preston <prestonvictor25@gmail.com>
2023-08-24 06:50:00 -0700

    feat: implement command execution engine and PATH resolution system

---
+++ path.c
@@ -0,0 +1,29 @@
+#include "path.h"
+
+/**
+  * search_path - searches for the directory with the executable program
+  * @info: argument passed
+  * @path: argument passed
+  * Return: pointer to directory string
+  */
+char *search_path(info_t *info, list_t *path)
+{
+	char *pathname, *command = *info->tokens;
+	struct stat sb;
+
+	while (path)
+	{
+		if (*path->str == '\0')
+			pathname = strjoin(NULL, "/", info->cwd, command);
+		else
+			pathname = strjoin(NULL, "/", path->str, command);
+		if (stat(pathname, &sb) == 0)
+		{
+			if ((sb.st_mode & S_IFMT) != S_IFDIR)
+				return (pathname);
+		}
+		free(pathname);
+		path = path->next;
+	}
+	return (NULL);
+}
